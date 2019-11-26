ba2a2c76d6c23fea91ce5e033b3fe6bb96204df9
slsay <shengli1222@163.com>
2019-11-26 14:20:11 +0800

    Update db.js
    修改默认端口

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ module.exports = {
   connections: {
     issue: {
       host: process.env.DB_HOST || "127.0.0.1",
-      port: process.env.DB_PORT || 29000,
+      port: process.env.DB_PORT || 27017,
       user: process.env.DB_USER,
       pass: process.env.DB_PASS,
       dbName: process.env.DB_NAME || "issue",
