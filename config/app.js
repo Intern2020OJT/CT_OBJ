c286e5afe17be8c53e910e5faa793bb760f73527
airoucat <xuanyuantec@gmail.com>
2019-11-27 15:07:21 +0800

    test

--- config/app.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "issues",
-  alias: "issues analytics",
-  port: process.env.PORT || 3000,
-  tokenHeader: "x-token-issues",
-  tokenSecret: process.env.TOKEN || "issues",
-  tokenLength: 32,
-  tokenExpires: 24 * 60 * 60 * 1000, // 24h
-};
--- src/core/response.js
+++
@@ -1,7 +0,0 @@
-exports.sendSuccess = (res, result) => {
-  res.json({ success: true, data: result });
-};
-
-exports.sendError = (res, err) => {
-  res.status(err.status).json({ success: false, errorCode: err.status, errorMessage: err.message });
-};
