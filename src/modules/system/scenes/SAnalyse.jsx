6d87f4c47a88d00f88daf455b5317c7db61db8cc
AyaseEUmi <1055241803@qq.com>
2019-12-05 12:52:45 +0800

    将上面部分放入SAnalyse

--- src/modules/system/scenes/SAnalyse.jsx
+++ src/modules/system/scenes/SAnalyse.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import SOverallAnalyse from './SOverallAnalyse';
 import SClassifyAnalyse from './SAnalyseRows/SClassifyAnalyse/SClassifyAnalyse';
 
 const SAnalyse = () => {
   return (
     <div className="Analyse">
+      <SOverallAnalyse />
       <SClassifyAnalyse />
     </div>
   );
--- src/modules/system/scenes/SHome.jsx
+++ src/modules/system/scenes/SHome.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import SOverallAnalyse from './SOverallAnalyse';
 import SAnalyse from './SAnalyse';
 
 const SHome = () => {
   return (
     <div className="home">
-      <SOverallAnalyse />
       <SAnalyse />
     </div>
   );
