d96780aed63b79bb9f2d5e698faacef8b7e53f67
Shanxiaolin <55526635+Shanxiaolin@users.noreply.github.com>
2019-12-12 14:46:28 +0800

    Create ctrl_issues.js

---
+++ src/modules/system/controllers/ctrl_issues.js
@@ -0,0 +1,20 @@
+const log = require("../../../core/logger"); 	
+exports.openingissues = async (req) => {	
+    log.info("get openingissues");	
+    try{	
+      const data = [	
+        { genre: 'Sports', sold: 8, income: 2300 },	
+        { genre: 'Strategy', sold: 8, income: 667 },	
+        { genre: 'Action', sold: 9, income: 982 },	
+        { genre: 'Shooter', sold: 6, income: 5271 },	
+        { genre: 'Other', sold: 7, income: 3710 }	
+      ]  	
+        res =data;	
+        return (res);	
+      }	
+    catch(err){	
+      log.info("get openingissues err")	
+      log.err(err)	
+      throw err	
+    }	
+  }
