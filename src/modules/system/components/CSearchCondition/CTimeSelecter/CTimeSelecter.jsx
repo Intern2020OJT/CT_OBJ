2902f21d1e1326d35cea033e252345f39ec4d4e6
AyaseEUmi <1055241803@qq.com>
2019-12-18 15:32:59 +0800

    得到了期间

--- src/modules/system/components/CSearchCondition/CTimeSelecter/CTimeSelecter.jsx
+++ src/modules/system/components/CSearchCondition/CTimeSelecter/CTimeSelecter.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Card, DatePicker } from 'antd';
 
 const { RangePicker } = DatePicker;
+function onChange(date, dateString) {
+  console.log(dateString); // 给杨
+}
 const CTimeSelecter = () => {
   return (
     <Card style={{ width: '1220px', height: '150px' }}>
       <p style={{ fontSize: 18, margin: 20 }}>期间检索</p>
       <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', }}>
         <p style={{ fontSize: 20 }}>期间：</p>
-        <RangePicker placeholder={['开始时间', '结束时间']} />
+        <RangePicker onChange={onChange} placeholder={['开始时间', '结束时间']} />
       </div>
     </Card>
   );
