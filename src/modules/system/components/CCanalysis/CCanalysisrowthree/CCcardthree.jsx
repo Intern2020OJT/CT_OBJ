736b2d9795951d59e5072a84c3e74c9d01e5974d
Shanxiaolin <Shanxiaolin163@163.com>
2019-12-18 12:50:44 +0800

    client/sxl

--- src/modules/system/components/CCanalysis/CCanalysisrowthree/CCcardthree.jsx
+++ src/modules/system/components/CCanalysis/CCanalysisrowthree/CCcardthree.jsx
@@ -1,36 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { Card } from 'antd';
+
 import { get } from '../../../../../utils/fetch';
-import React, { useState,useEffect } from 'react';
-import {Card} from 'antd';
-import CCpolythree from './CCpolythree'
+
 import { API_CARDTHREE } from '../../../../../utils/constants';
+
+import CCpolythree from './CCpolythree';
+
+
 const CCcardthree = () => {
   const [data, setData] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const data = await get(API_CARDTHREE)
+      // eslint-disable-next-line no-shadow
+      const res = [{ name: 'CT_OBJ' }, { name: 'ISE_OBJ' }, { name: 'SMD_OBJ' }];
+      // eslint-disable-next-line no-shadow
+      const data = await get(API_CARDTHREE, { res });
       setData(data);
-    }
+    };
     fetchData();
   }, []);
-      const [tabs, setState] = useState('bar');
-      const tabList = [
-        {
-          key: 'bar',
-          tab: '折线图',
-        } 
-      ];
-      const contentList = {
-        bar: <CCpolythree data={data}/>,
-      };
+  // eslint-disable-next-line no-unused-vars
+  const [tabs, setState] = useState('bar');
+  const tabList = [
+    {
+      key: 'bar',
+      tab: '折线图',
+    } 
+  ];
+  const contentList = {
+    bar: <CCpolythree data={data} />,
+  };
     
-      return (
-        <Card
-          style={{ width: '1200px', height: '500px' }}
-          title="多项目issues参与人员分析"
-          tabList={tabList}
-        >
-          {contentList[tabs]}
-        </Card>
-      );
-    };
+  return (
+    <Card
+      style={{ width: '1200px', height: '500px' }}
+      title="多项目issues参与人员分析"
+      tabList={tabList}
+    >
+      {contentList[tabs]}
+    </Card>
+  );
+};
 export default CCcardthree;
