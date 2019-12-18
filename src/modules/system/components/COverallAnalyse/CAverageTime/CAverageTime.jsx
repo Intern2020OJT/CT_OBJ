afbd201c6ee41858d35c6cb18ed8bb0f607b6f73
AyaseEUmi <1055241803@qq.com>
2019-12-18 15:18:55 +0800

    标准格式

--- src/modules/system/components/COverallAnalyse/CAverageTime/CAverageTime.jsx
+++ src/modules/system/components/COverallAnalyse/CAverageTime/CAverageTime.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Statistic,Card } from 'antd';
+import { Statistic, Card } from 'antd';
 
 const CAverageTime = () => {
-    return (
-        <Card style={{border:'1px solid #D9D9D9'}}>
-            <p style={{ fontSize: 18, margin: 20 }}>平均对应时间</p>
-            <Statistic  
-                style={{ width: '283px', height: '80px' }}
-                valueStyle={{textAlign:'center',color:'green',fontSize:40}}     
-                value={12.191} //待修改
-                precision={2}
-                suffix={<span style={{fontSize:35}}> h</span>}
-            />
-        </Card>
-    )
-}
+  return (
+    <Card style={{ border: '1px solid #D9D9D9' }}>
+      <p style={{ fontSize: 18, margin: 20 }}>平均对应时间</p>
+      <Statistic  
+        style={{ width: '283px', height: '80px' }}
+        valueStyle={{ textAlign: 'center', color: 'green', fontSize: 40 }}     
+        value={12.191} // 待修改
+        precision={2}
+        suffix={<span style={{ fontSize: 35 }}> h</span>}
+      />
+    </Card>
+  );
+};
 
 export default CAverageTime;
--- src/modules/system/components/COverallAnalyse/CRetentionRate/CRetentionRate.jsx
+++ src/modules/system/components/COverallAnalyse/CRetentionRate/CRetentionRate.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { Statistic,Card,Icon } from 'antd';
+import { Statistic, Card, Icon } from 'antd';
 
 const CRetentionRate = () => {
-    return (
-        <Card style={{border:'1px solid #D9D9D9'}}>
-            <p style={{ fontSize: 18, margin: 20 }}>Issues 滞留率</p>
-            <Statistic  
-                style={{ width: '283px', height: '80px' }}
-                valueStyle={{textAlign:'center',color:'green',fontSize:40}}     
-                value={5}//待修改 
-                prefix={<Icon type="left" style={{fontSize:25}} />}
-                suffix={<span style={{fontSize:25}}> %</span>}
-            />
-        </Card>
-    )
-}
+  return (
+    <Card style={{ border: '1px solid #D9D9D9' }}>
+      <p style={{ fontSize: 18, margin: 20 }}>Issues 滞留率</p>
+      <Statistic  
+        style={{ width: '283px', height: '80px' }}
+        valueStyle={{ textAlign: 'center', color: 'green', fontSize: 40 }}     
+        value={5}// 待修改 
+        prefix={<Icon type="left" style={{ fontSize: 25 }} />}
+        suffix={<span style={{ fontSize: 25 }}> %</span>}
+      />
+    </Card>
+  );
+};
 
 export default CRetentionRate;
