414a267297ddb3f85d728c696a8f3bce8feead10
Shanxiaolin <Shanxiaolin163@163.com>
2019-12-18 10:24:34 +0800

    server/sxl

--- src/modules/system/controllers/ctrl_canalysis.js
+++ src/modules/system/controllers/ctrl_canalysis.js
@@ -1,32 +1,32 @@
 const log         = require("../../../core/logger");
 const ModelIntrosIssues = require("../models/mod_introsDataIssues");
 const ModelIntros    = require("../models/mod_introsData");
-//存数据 
+// 存数据
 exports.creatcanalysis = async (test) => {
-  try {    
+  try {
     await Promise.all(test.map(async (record) => {
-      await ModelIntros.create({...record});
+      await ModelIntros.create({ ...record });
     }));
-     } catch (err) {
+  } catch (err) {
     throw err;
   }
 };
 exports.creat = async (test) => {
-  try {    
+  try {
     await Promise.all(test.map(async (record) => {
-      await  ModelIntrosIssues.create({...record});
+      await  ModelIntrosIssues.create({ ...record });
     }));
-     } catch (err) {
+  } catch (err) {
     throw err;
   }
 };
-//取数据
-exports.canalysisissues = async(prop) => {
+// 取数据
+exports.canalysisissues = async (prop) => {
   log.info("get canalysis start.");
   try {
-    const id = {"name":prop}
-    const projection = {}
-    const message = await ModelIntrosIssues.getList(id,projection);    
+    const id = { name:prop };
+    const projection = {};
+    const message = await ModelIntrosIssues.getList(id, projection);
     return message;
   } catch (err) {
     log.info("get canalysis start.");
@@ -34,12 +34,12 @@ exports.canalysisissues = async(prop) => {
     throw err;
   }
 };
-exports.canalysis = async(prop) => {
+exports.canalysis = async (prop) => {
   log.info("get canalysis start.");
   try {
-    const id = {"name":prop}
-    const projection = {}
-    const message = await ModelIntros.getList(id,projection);    
+    const id = { name:prop };
+    const projection = {};
+    const message = await ModelIntros.getList(id, projection);
     return message;
   } catch (err) {
     log.info("get canalysis start.");
@@ -47,12 +47,12 @@ exports.canalysis = async(prop) => {
     throw err;
   }
 };
-exports.canalysisauth = async(prop) => {
+exports.canalysisauth = async (prop) => {
   log.info("get canalysis start.");
   try {
-    const m = {name:prop}
-    const g = {_id: "$user.login" , number:{ $sum: 1}}
-    const message = await ModelIntrosIssues.aggregate(m,g);  
+    const m = { name:prop };
+    const g = { _id: "$user.login", number:{ $sum: 1 } };
+    const message = await ModelIntrosIssues.aggregate(m, g);
     return message;
   } catch (err) {
     log.info("cuowu");
