ab3bab8621b2021ad9b3656582175ec4171fc08d
AyaseEUmi <1055241803@qq.com>
2019-12-19 14:18:52 +0800

    删去open时间计算

--- src/modules/system/controllers/ctrl_overallAnalyse.js
+++ src/modules/system/controllers/ctrl_overallAnalyse.js
@@ -6,7 +6,7 @@ exports.overallAnalyse = async (req) => {
   log.info("get issues start.");
   try {
     const overallDatas = {};
-    const condition = { name: req.query.prjName }; // 此处需要改成从前端获取项目名
+    const condition = { name: req.query.prjName }; 
     const projection = {};
     const project = await ModelIntros.getList(condition, projection); // 项目
     const issues = await ModelIntrosIssues.getList(condition, projection); // 项目的issues
@@ -18,10 +18,6 @@ exports.overallAnalyse = async (req) => {
         const closedAt = new Date(issues[i].closed_at);
         const createdAt = new Date(issues[i].created_at);
         totalTime += closedAt.getTime() - createdAt.getTime();
-      } else {
-        const updatedAt = new Date(issues[i].updated_at);
-        const createdAt = new Date(issues[i].created_at);
-        totalTime += updatedAt.getTime() - createdAt.getTime();
       }
     }
     overallDatas.averageTime = totalTime / overallDatas.total / 36000000; // issue平均对应时长
