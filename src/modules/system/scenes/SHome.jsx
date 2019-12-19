2d332b8d63271c8dc5d2a0452c2321c1212e0dd2
烤金鱼 <programmer.yang@foxmail.com>
2019-12-19 12:32:50 +0800

    一些修改

--- src/modules/system/scenes/SHome.jsx
+++ src/modules/system/scenes/SHome.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 import SAnalyse from './SAnalyse';
@@ -9,15 +9,16 @@ import SAnalyseHeader from './SAnalyseHeader';
 
 const SHome = (props) => {
   const projectName = props.location.state.pullData;
-  const [state, setState] = useState();
+  const [time, setTime] = useState([]);
   const getTime = (res) => {
     console.log(projectName.name);
     console.log(res);
   };
+
   return (
     <div className="home">
       <SAnalyseHeader func={getTime} />
-      <SAnalyse />
+      <SAnalyse time={time} objName={projectName.name} />
     </div>
   );
 };
