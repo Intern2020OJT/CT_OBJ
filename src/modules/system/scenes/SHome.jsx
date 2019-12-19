d37551e7d4e949135d3f11dbf5147b83d5d6c419
AyaseEUmi <1055241803@qq.com>
2019-12-19 09:20:59 +0800

    前端显示

--- src/modules/system/scenes/SHome.jsx
+++ src/modules/system/scenes/SHome.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 
 import SAnalyse from './SAnalyse';
+import SAnalyseHeader from './SAnalyseHeader';
 
 
 const SHome = (props) => {
-  const getData=props.location.state.pullData
+  // const getData = props.location.state.pullData
   return (
     <div className="home">
+      <SAnalyseHeader />
       <SAnalyse />
     </div>
   );
