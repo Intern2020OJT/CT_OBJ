8723488a8f9fa99fd2daa8da65b89000becb3e8e
AyaseEUmi <1055241803@qq.com>
2019-12-19 14:43:06 +0800

    再次过滤

--- src/modules/system/controllers/ctrl_overallAnalyse.js
+++ src/modules/system/controllers/ctrl_overallAnalyse.js
@@ -1,6 +1,5 @@
 const log = require("../../../core/logger");
 const ModelIntrosIssues = require("../models/mod_introsDataIssues");
-const ModelIntros = require("../models/mod_introsData");
 
 exports.overallAnalyse = async (req) => {
   log.info("get issues start.");
@@ -8,18 +7,24 @@ exports.overallAnalyse = async (req) => {
     const overallDatas = {};
     const condition = { name: req.query.prjName }; // 项目名
     const projection = {};
-    const project = await ModelIntros.getList(condition, projection); // 项目
     const issues = await ModelIntrosIssues.getList(condition, projection); // 项目的issues
-    overallDatas.total = issues.length; // issue总数
-    overallDatas.opening = project[0].open_issues_count;  // open的issue数
     let totalTime = 0;
+    let total = 0;
+    let opening = 0;
     for (let i = 0; i < issues.length; i++) {
-      if (issues[i].state === "closed" && issues[i].pull_request === undefined) {
-        const closedAt = new Date(issues[i].closed_at);
-        const createdAt = new Date(issues[i].created_at);
-        totalTime += closedAt.getTime() - createdAt.getTime();
+      if (issues[i].pull_request === undefined) {
+        total++;
+        if (issues[i].state === "closed") {
+          const closedAt = new Date(issues[i].closed_at);
+          const createdAt = new Date(issues[i].created_at);
+          totalTime += closedAt.getTime() - createdAt.getTime();
+        } else {
+          opening++;
+        }
       }
     }
+    overallDatas.total = total; // issue总数
+    overallDatas.opening = opening;  // open的issue数
     overallDatas.averageTime = totalTime / overallDatas.total / 36000000; // issue平均对应时长
     overallDatas.retentionRate = (overallDatas.opening / overallDatas.total) * 100; // 滞留率
     return overallDatas;
